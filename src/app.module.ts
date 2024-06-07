8a504948f05521eeb4ffa7f9b5d197ee0924c415
Cristhofer Umbo <cristoferariz123455@correo.com>
2024-06-06 21:16:30 -0500

    change_credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { FirebaseModule } from './firebase/firebase.module';
       host: process.env.MYSQL_HOST || 'localhost',
       port: parseInt(process.env.MYSQL_PORT),
       username: process.env.MYSQL_USERNAME || 'root',
-      password: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
